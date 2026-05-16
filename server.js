cbd94e02a6d8b585cb5b4437ca3f2a2cb722b5df
Maksym Slipkevych <maxymslipkewych@gmail.com>
2026-05-17 01:38:59 +0300

    Fix 13

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch(err => console.error('- Помилка MongoDB:', err));
 
 // Запуск сервера
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Сервер запущено на порту ${PORT}`);
 });
