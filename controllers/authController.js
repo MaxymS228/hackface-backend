bab8ad7cbd29dc8e0df2bec983b1e454e4c708d2
Maksym Slipkevych <maxymslipkewych@gmail.com>
2026-05-17 00:44:06 +0300

    Fix 9

--- controllers/authController.js
+++ controllers/authController.js
@@ -56,25 +56,9 @@ const registerUser = async (req, res) => {
       `
     };
 
-    res.status(201).json({ message: 'Користувач зареєстрований. Лист відправлено.' });
-
     transporter.sendMail(mailOptions)
       .then(() => console.log('Лист відправлено:', newUser.email))
       .catch(err => console.error('Помилка листа:', err.message));
-
-
-    // resend.emails.send({
-    //   from: 'Hackathon Face <onboarding@resend.dev>',
-    //   to: newUser.email,
-    //   subject: 'Підтвердження реєстрації в Hackathon Face',
-    //   html: `
-    //     <h2>Вітаємо, ${newUser.name}!</h2>
-    //     <p>Дякуємо за реєстрацію. Будь ласка, підтвердіть вашу електронну пошту, перейшовши за посиланням нижче:</p>
-    //     <a href="${verificationUrl}" style="padding: 10px 20px; background-color: #4f46e5; color: white; text-decoration: none; border-radius: 5px;">Підтвердити пошту</a>
-    //     <p>Якщо кнопка не працює, скопіюйте це посилання у браузер: <br/> ${verificationUrl}</p>
-    //   `
-    // }).then(() => console.log('Лист відправлено:', newUser.email))
-    //   .catch(err => console.error('Помилка листа:', err.message));
 
   } catch (error) {
     console.error('Помилка реєстрації:', error);
